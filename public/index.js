b0c8460706ae5253874165ca814e69d51f009ba6
Macpresto <mcmpresto25@gmail.com>
2023-02-20 15:34:34 +0800

    add collapse function for territories

---
+++ public/index.js
@@ -0,0 +1,32 @@
+//const list = document.querySelectorAll('li');
+const toggler = document.querySelectorAll('.caret');
+const ulGroup = document.querySelectorAll('ul');
+const liGroup = document.querySelectorAll('li');
+const parentUl = document.querySelector('#myUL');
+
+// ulGroup.forEach((data) => {
+//     console.log(data.id);
+//     data.classList.add("list-group-flush", "mx-auto");
+// });
+
+parentUl.classList.add("list-group-flush", "mx-auto");
+
+liGroup.forEach((data) => {
+data.classList.add("list-group-item");
+
+if (!data.children.length > 0){
+          data.classList.add("root");
+}
+});
+
+toggler.forEach(toggle => {
+          toggle.addEventListener('click', () => {
+                    //toggle.parentElement.classList.toggle("active");
+                    toggle.nextElementSibling.classList.toggle("active");
+                    toggle.classList.toggle("active");
+
+          });
+});
+
+
+//console.log(listlist);
