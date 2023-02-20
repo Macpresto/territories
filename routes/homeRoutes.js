d67f4191453e925f5b51a2ff6e3a5a41cbf26eb3
Macpresto <mcmpresto25@gmail.com>
2023-02-20 15:46:51 +0800

    add authentication middleware to home routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const homeControllers = require('../controllers/homeControllers');
+const isAuthenticated = require('../middlewares/authentication');
 
 
 
-router.get('/home/index', homeControllers.list_territories);
+
+router.get('/home/index', isAuthenticated,homeControllers.list_territories);
 
 module.exports = router;
