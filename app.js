f65cef3617519817f8b7cb70e7dd5f3f65792538
Macpresto <mcmpresto25@gmail.com>
2023-02-20 12:42:11 +0800

    add page routes to app

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const morgan = require('morgan');
+
+//view engine
+app.set('view engine', 'ejs');
+
+//fire app
+app.listen(3000);
+
+//middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.use(session({
+          secret: 'test',
+          cookie: { maxAge: 3000000 },
+          saveUninitialized: false,
+          
+      }));
+      
+//routes
+app.get('/', (req,res) =>{
+          res.redirect('/home/index');
+});
+
+app.use(accountRoutes);
+app.use(homeRoutes);
+
+app.use((req,res) => {
+          res.status(404).render('404', { title: '404' });
+});
+      
