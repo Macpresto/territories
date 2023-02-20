4873241f2279050d22fe1f4978ff6cdb1e93f39d
Macpresto <mcmpresto25@gmail.com>
2023-02-20 13:24:32 +0800

    add API login

--- controllers/accountControllers.js
+++ controllers/accountControllers.js
@@ -1,8 +1,32 @@
+const { default: axios } = require("axios");
+
 const login_page = (req,res) =>{
+          console.log('im here=e');
           res.render('login', { title : 'Login', error : null });
       };
 
 
+const account_login = (req, res) => {
+    const { username, password } = req.body;
+
+    axios.post('https://netzwelt-devtest.azurewebsites.net/Account/SignIn', { username, password })
+    .then(response => {
+                console.log(`this is the status ${response.status}`);
+                if(response.status === 200) {
+                console.log('User Logged in');
+                req.session.user = req.body.username;
+                res.redirect('/');
+                } else {
+                res.render('login', { title : 'Login', error : 'invalid credentials'});
+                }
+    })
+    .catch(err => {
+                console.log(err);
+                res.redirect('/account/login');
+    });
+};
+
 module.exports = {
-          login_page
+          login_page,
+          account_login
 }
