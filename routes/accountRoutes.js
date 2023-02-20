c26fc2fb537bf643c7ba1db6cea7a3499d20de77
Macpresto <mcmpresto25@gmail.com>
2023-02-20 13:25:01 +0800

    fix login routes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const accountControllers = require('../controllers/accountControllers');
 
 router.get('/account/login', accountControllers.login_page);
-router.post('/account/login', accountControllers.login_page);
-
+router.post('/account/login', accountControllers.account_login);
 
 
 module.exports = router;
